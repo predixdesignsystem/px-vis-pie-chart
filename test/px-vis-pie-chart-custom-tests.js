a84a838d64d34bdf6ec854468ace3cb59ad7714e
Benoit Chevalier <benoit.chevalier@ge.com>
2016-07-12 11:40:15 -0700

    refine title palcement

--- test/px-vis-pie-chart-custom-tests.js
+++ test/px-vis-pie-chart-custom-tests.js
@@ -1,27 +1,15 @@
 // This is the wrapper for custom tests, called upon web components ready state
 function runCustomTests() {
   // Place any setup steps like variable declaration and initialization here
-
+  var pie = document.getElementById('pie'),
+      donut = document.getElementById('donut');
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
-  suite('Custom Automation Tests for px-vis-pie-chart', function() {
-    test('Check initial value of counter', function(done){
-      var counterEl = Polymer.dom(document).querySelector('px-vis-pie-chart'),
-          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
-      assert.equal(counterValueEl.textContent, '0');
-      done();
+  suite('Pie tests', function() {
+
+    test('bof', function() {
+
     });
 
-    test('Clicking px-vis-pie-chart increments the counter', function(done){
-      var counterEl = Polymer.dom(document).querySelector('px-vis-pie-chart'),
-          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
-      assert.equal(counterValueEl.textContent, '0');
-
-      counterEl.click();
-      flush(function(){
-        assert.equal(counterValueEl.textContent, '1');
-      });
-      done();
-    });
   });
 };
