b82d540512df6cb563ae59369f21a8e52d77bdc1
Runn Vermel <runn.vermel@ge.com>
2017-01-10 14:30:48 -0800

    trying to delete files with find

--- scripts/ghp.sh
+++ scripts/ghp.sh
@@ -35,7 +35,7 @@ cd $TRAVIS_BUILD_DIR
 REPO_NAME=$(grep "name" bower.json | sed 's/"name": "//' | sed 's/",//')
 echo "repo name is ${REPO_NAME}"
 
-rm * $TRAVIS_BUILD_DIR
+find $TRAVIS_BUILD_DIR -type f -maxdepth 1 -exec rm -fv {} \;
 find $TRAVIS_BUILD_DIR ! -name '.git' -type d -exec rm -rf {} +
 
 
